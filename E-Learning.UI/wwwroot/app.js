a6ee9bf0dc49c199f6fd5e9391a5099780e06242
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-10 15:58:59 +0500

    Vashshe yorvation bo'p ketdi

---
+++ E-Learning.UI/wwwroot/app.js
@@ -0,0 +1,6 @@
+var darkModeToggle = document.getElementById('darkModeToggle');
+var body = document.body;
+
+darkModeToggle.addEventListener('click', function () {
+    body.classList.toggle('dark-mode');
+});
